fa0ee87fc2752d38ce095b920ade58b5b249eaad
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2019-08-31 14:50:39 +0900

    ym-sat の更新に伴う変更

--- py-src/sample.py
+++ py-src/sample.py
@@ -7,14 +7,14 @@
 ### Copyright (C) 2019 Yusuke Matsunaga
 ### All rights reserved.
 
-from ym_sat import Literal, VarId, SatBool3, SatSolver, SatSolverType, Stats
+from ym_sat import Literal, Bool3, Solver, SolverType, Stats
 
 # SATソルバーの生成
 # どのSATソルバを使うかの指定もできるがとりあえず省略
-sol = SatSolver()
+sol = Solver()
 
 # 変数の生成
-# 実はここで返される型は変数(VarId)ではなくリテラル(Literal)である．
+# 実はここで返される型は変数ではなくリテラル(Literal)である．
 # 正のリテラルと変数はほぼ等しいが，リテラルは否定できるが変数は否定できない．
 # 論理式では変数ではなくリテラルを用いる．
 v1 = sol.new_variable()
@@ -35,10 +35,10 @@ sol.add_clause( c2 )
 sol.add_clause( (v2,) )
 
 # 求解
-# stat は SatBool3 型で SatBool3._True, SatBool3._False, SatBool3._X のいずれか．
+# stat は Bool3 型で Bool3._True, Bool3._False, Bool3._X のいずれか．
 # 通常は _True (SAT) か _False(UNSAT) になる．_X は途中で探索を打ち切った場合．
-# model は SatBool3 のリスト．SATの場合の各々変数の割り当て結果が格納される．
-# UNSAT の場合には mode は None となる．
+# model は Bool3 のリスト．SATの場合の各々変数の割り当て結果が格納される．
+# UNSAT の場合には model は None となる．
 stat, model = sol.solve()
 
 print('stat = {}'.format(stat))
